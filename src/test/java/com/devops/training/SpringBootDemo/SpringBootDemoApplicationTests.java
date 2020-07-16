7d6bf4d0a4d335ff9e6bf7e21dbee3be0574e21f
Sudesh Duvuru <sudeshduvuru@Sudeshs-MacBook-Pro.local>
2020-07-16 12:27:05 +0100

    SpringBootDemo

---
+++ src/test/java/com/devops/training/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.training.SpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
